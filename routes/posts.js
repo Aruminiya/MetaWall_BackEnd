b851633a15d9e0d3775285abd811794349a954d1
Leo <leo80103@gmail.com>
2024-04-28 13:54:17 +0800

    把錯誤處理弄更細

--- routes/posts.js
+++ routes/posts.js
@@ -2,64 +2,72 @@ const express = require('express');
 const router = express.Router();
 const Post = require("../models/postModel.js");
 // const User = require("../models/userModel.js");
-const handleError = require('../handleError.js');
-const handleSuccess = require('../handleSuccess.js');
 const appError = require('../errorHandeler/appError.js');
 const handleErrorAsync = require('../errorHandeler/handleErrorAsync.js');
 
 
 // 取得貼文
-router.get('/',async function(req, res, next) {
-  if(req.query.q){
+router.get('/',handleErrorAsync(
+  async function(req, res, next) {
     const getPost = await Post.find(req.query).populate({
       path:'user',
-      slect:'email'
+      select:'email'
     });
-    handleSuccess(res, getPost);
-  }else{
-    next(appError(400,"沒有寫 query.q"));
-  }
-  
-});
+    res.status(200).json({
+      "status":"success",
+      data: getPost
+    });
+}
+));
 
 // 新增貼文
 router.post('/',handleErrorAsync(
   async function(req, res, next) {
     const toPost = await Post.create(req.body);
-    handleSuccess(res, toPost);
+    res.status(200).json({
+      "status":"success",
+      data: toPost
+    });
   }
 ));
 
 // 刪除所有貼文
-router.delete('/',async function(req, res, next) {
-  try{
+router.delete('/',handleErrorAsync(
+  async function(req, res, next) {
     await Post.deleteMany({});
-    handleSuccess(res, []);
-  }catch(err){
-    handleError(res, err, "刪除所有貼文資料失敗");
+    res.status(200).json({
+      "status":"success",
+      data: []
+    });
   }
-});
+));
 
 // 刪除單一貼文
-router.delete('/:id',async function(req, res, next) {
-  try{
+router.delete('/:id',handleErrorAsync(
+  async function(req, res, next) {
     const { id } = req.params
     const deletePost = await Post.findByIdAndDelete(id);
-    handleSuccess(res, deletePost);
-  }catch(err){
-    handleError(res, err, "刪除單一貼文資料失敗");
+    if(deletePost){
+      res.status(200).json({
+        "status":"success",
+        data: deletePost
+      });
+    }else{
+      return next(appError(400,"找不到該刪除資料"))
+    }
   }
-});
+));
 
 // 編輯單一貼文
-router.patch('/:id',async function(req, res, next) {
-  try{
+router.patch('/:id',handleErrorAsync(
+  async function(req, res, next) {
     const { id } = req.params
     const editPosts = await Post.findByIdAndUpdate(id,req.body,{ new: true });
-    handleSuccess(res, editPosts);
-  }catch(err){
-    handleError(res, err, "編輯單一貼文資料失敗");
+    res.status(200).json({
+      "status":"success",
+      data: editPosts
+    });
   }
-});
+));
 
 module.exports = router;
